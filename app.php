0b8461a6e107d7c737ef96d0ad72eb75190bc498
Luis Palma <luisp3230@gmail.com>
2014-01-24 15:58:19 +0000

    agregamos otra operacion

--- app.php
+++ app.php
@@ -6,7 +6,7 @@ use Hackspace\Suma;
 #operacion1 suma 3, 4
 #operacion2 suma 5, 8
 
-$suma=new Suma(3,4);
-echo (string)$suma->ejecutar();
+$suma=new Suma(5,8);
+echo $suma->ejecutar();
 echo "\n";
 
